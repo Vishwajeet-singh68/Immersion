16f91e345f246ae551a6dac5395ee36fe1898c67
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-06-09 11:41:54 +0530

    prime

--- prime.java
+++ prime.java
@@ -3,11 +3,12 @@ public class prime{
 public static void main(String args[]){
 Scanner in = new Scanner(System.in);
 int n=in.nextInt();
+int count=0;
 for(int i=2;i<=n;i++){
 if(n%i==0)
 	count++;
 }
-if(count==1){
+if(count==1)
 	System.out.println(n+" is prime Number");
 else
 	System.out.println("Not prime");
